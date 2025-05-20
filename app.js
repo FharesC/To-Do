46cb6630b7330e658f7f66f7598fa6848cfb8d10
Fhares Colón <fharescolon@gmail.com>
2025-05-20 14:17:49 -0600

    Buscador

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ let a = "jajaj";
 let tareas = [];
 let filtro = document.querySelector("#filter");
 let allLI = document.querySelectorAll("li");
+let buscador = document.querySelector("#Buscador");
+let tareasyFiltros = tareas
 
+//Este es el botón que dice enviar, donde al igual que con el Enter, se va a colocar el contenido
 boton.addEventListener("click", (event) => {
   if (String(enter.value.trim()) == "") {
   } else {
@@ -13,131 +16,108 @@ boton.addEventListener("click", (event) => {
   }
 });
 
+// acá solo es el evento donde esta el array, que avisa del texto y si está completado o no (false, true)
 
 let agregar = (event) => {
   let nuevaTarea = {
     texto: enter.value,
     isComplete: false,
+    key: parseInt(Math.random()*10000)    
   };
-
+  console.log(nuevaTarea.key);
+  
   tareas.push(nuevaTarea);
 
-  crearElementosyRenderizar(nuevaTarea.texto)
-}
+  crearElementosyRenderizar(nuevaTarea);
+};
 
+//para avisar que el evento que se va a inspeccionar va a ser en event.target.value que es donde se da el value
+filtro.addEventListener("change", (event) => {
+  console.log(event.target.value);
+  filtroTareas(event.target.value);
+});
 
-  
-
-
-  const crearElementosyRenderizar = (valor) => {
-    let li = document.createElement("li");
+//constante donde están todos los elementos que se van a agregar
+const crearElementosyRenderizar = (tarea) => {
+  let li = document.createElement("li");
   let div = document.createElement("div");
   let input = document.createElement("input");
   let btn1 = document.createElement("button");
   let btn2 = document.createElement("button");
-  input.value = valor;
+  input.value = tarea.texto;
+  if (tarea.isComplete == true) {
+    input.classList.add("lineamedia");
+  }
   enter.value = "";
   input.setAttribute("readonly", true);
   div.classList.add("form-control");
   btn1.className = "bi bi-check-all text-light fs-3 fondo2";
   btn2.className = "bi bi-trash text-light fs-3 fondo2";
-  input.className = "white";
+  input.classList.add("white");
   div.className = "PR";
   li.appendChild(div);
   div.appendChild(input);
   div.appendChild(btn1);
   div.appendChild(btn2);
   ul.appendChild(li);
- 
 
+  //este es el botón que va a servir para borrar
   btn2.addEventListener("click", (event) => {
     ul.removeChild(li);
-    tareas = tareas.filter(
-      (item) =>
-        item.texto !=
-        event.target.previousElementSibling.previousElementSibling.value
-    );
+    tareasyFiltros = tareasyFiltros.filter((item) =>item.key !=tarea.key);
+    
     console.log(tareas);
   });
 
+  //Este es para dar click en el check y que se coloque la línea media cuando ya está completo
   btn1.addEventListener("click", (event) => {
     input.classList = "lineamedia";
-    tareas = tareas.map((item) => {
-      if (item.texto == event.target.previousElementSibling.value) {
+    tareasyFiltros = tareasyFiltros.map((item) => {
+      if (item.key == tarea.key) {
         item.isComplete = true;
       }
       return item;
     });
     console.log(tareas);
   });
+};
 
 
+//Buscador
+let palabra = "";
+buscador.addEventListener("keyup", (event) => {
+  palabra = event.target.value;
+  if (palabra == "") {
+    filtro.value = 0
   }
-
-
-const filtroTareas = (value) => {
-  let filtro = [];
-  if (value == "pendientes") {
-    filtro = tareas.filter((item) => item.isComplete == false);
-  } else if (value == "completados") {
-    filtro = tareas.filter((item) => item.isComplete == true);
-  } else {
-    filtro = tareas;
-  }
+  tareasyFiltros = tareas.filter(item => item.texto.toLowerCase().includes(palabra))
+  
   ul.innerHTML = "";
-
-
-  filtro.forEach((item) => {
-    let li = document.createElement("li");
-    let div = document.createElement("div");
-    let input = document.createElement("input");
-    let btn1 = document.createElement("button");
-    let btn2 = document.createElement("button");
-    input.value = item.texto;
-    enter.value = "";
-    input.setAttribute("readonly", true);
-    div.classList.add("form-control");
-    btn1.className = "bi bi-check-all text-light fs-3 fondo2";
-    btn2.className = "bi bi-trash text-light fs-3 fondo2";
-    input.className = "white";
-    div.className = "PR";
-    li.appendChild(div);
-    div.appendChild(input);
-    if (value != "completados")  {
-      div.appendChild(btn1);
-    }
-    div.appendChild(btn2);        
-    ul.appendChild(li);
-
-    btn2.addEventListener("click", (event) => {
-      ul.removeChild(li);
-      tareas = tareas.filter(
-        (item) =>
-          item.texto !=
-          event.target.previousElementSibling.previousElementSibling.value
-      );
-      console.log(tareas);
-    });
-
-    btn1.addEventListener("click", (event) => {
-      input.classList = "lineamedia";
-      tareas = tareas.map((item) => {
-        if (item.texto == event.target.previousElementSibling.value) {
-          item.isComplete = true;
-        }
-        return item;
-      });
-      console.log(tareas);
-    });
+  tareasyFiltros.forEach((item) => {
+    crearElementosyRenderizar(item);
   });
-}
-
-filtro.addEventListener("change", (event) => {
-  console.log(event.target.value);
-  filtroTareas(event.target.value);
 });
 
 
+
+//Acá donde se filtra si ya está completado o si todavía no
+const filtroTareas = (value) => {
+  let filtro = [];
+  if (value == "pendientes") {
+    filtro = tareasyFiltros.filter((item) => item.isComplete == false);
+  } else if (value == "completados") {
+    filtro = tareasyFiltros.filter((item) => item.isComplete == true);
+  } else {
+    filtro = tareasyFiltros;
+  }
+  ul.innerHTML = "";
+
+  filtro.forEach((item) => {
+    crearElementosyRenderizar(item);
+  });
+};
+
+//Este es para que cuando le de enter, se coloque el contenido, y si está vacío que no pase nada
 enter.addEventListener("keydown", (evento) => {
   if (evento.key == "Enter") {
     if (String(enter.value.trim()) == "") {
